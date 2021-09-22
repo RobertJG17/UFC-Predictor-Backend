1af3fb34011d8ced9ca917b057028834a1896e63
RobertJG17 <robertjguerra@yahoo.com>
2021-09-21 22:13:50 -0700

    structured proper kaggle API client

--- kaggle_driver.py
+++ kaggle_driver.py
@@ -1,8 +1,18 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
 
 
 def download_data():
-    api = KaggleApi()
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    api_client = {
+        "username": os.environ.get('KAGGLE_USERNAME'),
+        "key": os.environ.get('KAGGLE_KEY')
+    }
+
+    api = KaggleApi(api_client=api_client)
     api.authenticate()
 
     api.dataset_download_file(
